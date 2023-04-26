66318cc601e9986596b27ba1d8e18cd4b32d06bb
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-04-26 16:18:36 +0200

    [ FirstCommit ] Vite Hello World

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default {
+data(){
+    return{
+        title : "La mia prima app con Vite!"
+    }
+}    
+}
+</script>
+
+<template>
+
+    <h1>{{ title }}</h1>
+  
+</template>
+
+<style>
+
+h1{
+    color: gold;
+    background-color: rgb(0, 19, 75);
+    padding: 20px;
+    border-radius: 15px;
+}
+</style>
